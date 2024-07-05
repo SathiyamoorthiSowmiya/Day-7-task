d1f5cadb90ff9953a2e4a8a4c630ba09f6ea6fb8
SathiyamoorthiSowmiya <Sowmimoorthi04@gmail.com>
2024-07-05 09:55:08 +0530

    updated

--- script.js
+++ script.js
@@ -44,9 +44,7 @@ fetch("https://restcountries.com/v3.1/all")
   .then((response) => response.json())
   .then((data) => {
     const totalPopulation = data.reduce(
-      (accumulator, country) => accumulator + country.population,
-      0
-    );
+      (accumulator, country) => accumulator + country.population,0);
     console.log("Total population of all countries:", totalPopulation);
   });
 
